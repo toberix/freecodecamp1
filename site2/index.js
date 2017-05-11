519cb5a27ee39e07dfe2a02dc6343d141cf05c2c
Fatumo Oluwatobi Ifeoluwa <tfatumo@emzorlan.com>
2017-05-11 16:08:42 +0100

    New Update

--- site2/index.js
+++ site2/index.js
@@ -1,110 +1,25 @@
-$(document).ready(function() {
-  $('i').hide();
-})
 
-$(window).load(function() {
-  $('i').show();
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-  var twitterPos = $('#twitter').position();
-  var githubPos = $('#github').position();
-  var stackPos = $('#stack').position();
-  var linkedinPos = $('#linkedin').position();
-  var codePos = $('#code').position();
-  var plusPos = $('#plus').position();
-  var mailPos = $('#mail').position();
-  var imgPos = $('.me').position();
-  
-  $('i').css({
-    position: 'absolute',
-    zIndex: '1',
-    top: imgPos.top + 100,
-    left: '47%'
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
-  
-  setTimeout(function() {
-    $('#twitter').animate({
-      top: twitterPos.top + 10,
-      left: twitterPos.left - 10
-    }, 500);
-  }, 250);
-  
-  setTimeout(function() {
-    $('#twitter').animate({
-      top: twitterPos.top,
-      left: twitterPos.left
-    }, 250);
-    
-    $('#github').animate({
-      top: githubPos.top + 10,
-      left: githubPos.left - 6
-    }, 500);
-  }, 500);
-  
-  setTimeout(function() {
-    $('#github').animate({
-      top: githubPos.top,
-      left: githubPos.left
-    }, 250);
-    
-    $('#stack').animate({
-      top: stackPos.top + 10,
-      left: stackPos.left - 3
-    }, 500);
-  }, 750);
-  
-  setTimeout(function() {
-    $('#stack').animate({
-      top: stackPos.top,
-      left: stackPos.left
-    }, 250);
-    
-    $('#linkedin').animate({
-      top: linkedinPos.top + 10,
-      left: linkedinPos.left
-    }, 500);
-  }, 1000);
-  
-  setTimeout(function() {
-    $('#linkedin').animate({
-      top: linkedinPos.top,
-      left: linkedinPos.left
-    }, 250);
-    
-    $('#code').animate({
-      top: codePos.top + 10,
-      left: codePos.left + 3
-    }, 500);
-  }, 1250);
-  
-  setTimeout(function() {
-    $('#code').animate({
-      top: codePos.top,
-      left: codePos.left
-    }, 250);
-    
-    $('#plus').animate({
-      top: plusPos.top + 10,
-      left: plusPos.left + 6
-    }, 500);
-  }, 1500);
-  
-  setTimeout(function() {
-    $('#plus').animate({
-      top: plusPos.top,
-      left: plusPos.left
-    }, 250);
-    
-    $('#mail').animate({
-      top: mailPos.top + 10,
-      left: mailPos.left + 10
-    }, 500);
-  }, 1750);
-  
-  setTimeout(function() {
-    $('#mail').animate({
-      top: mailPos.top,
-      left: mailPos.left
-    }, 250);
-  }, 2000);
-  
-})
+});
